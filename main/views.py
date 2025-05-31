803d6944ec266824028fd42bff6d6d5de53301d3
muharifs <muh.arifs1505@gmail.com>
2025-05-31 20:59:52 +0700

    make a navbar and 3 pages

--- main/views.py
+++ main/views.py
@@ -4,4 +4,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Saya pecundang yang baru belajar.")
+     return render(request, 'main/index.html')
+
+def about(request):
+    context = {
+        'title': 'Tentang Kami',
+        'message': 'Ini adalah halaman about.'
+    }
+    return render(request, 'main/about.html', context)
+
+def profile(request, username):
+    # Mengambil username dari parameter URL
+    usernameFormat = username.title() if username else 'Anonymous'
+    context = {
+        'username': usernameFormat,
+        'judul': 'Halaman Profile'
+    }
+    return render(request, 'main/profile.html', context)
